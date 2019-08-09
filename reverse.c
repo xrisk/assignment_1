a1ad351d5fd3ccaf2d0e3cc40e2fdffa6d470437
Rishav Kundu <rishav.kundu98@gmail.com>
2019-08-09 18:42:36 +0530

    Add bswap version

--- reverse.c
+++ reverse.c
@@ -2,6 +2,7 @@
 
 #include <err.h>
 #include <fcntl.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,17 +19,33 @@
     exit(EXIT_FAILURE);                                                        \
   } while (0)
 
-void write_message(const char *s) { write(STDOUT_FILENO, s, strlen(s)); }
+void write_message(const char *s) {
+  if (write(STDOUT_FILENO, s, strlen(s)) < 0)
+    handle_error("write to stdout failed");
+}
 
-void reverse_buffer(char *buffer, const size_t size) {
+void reverse_buffer(uint8_t *buffer, const size_t size) {
   for (size_t i = 0; i < size / 2; i++) {
-    char tmp = buffer[i];
+    uint8_t tmp = buffer[i];
     buffer[i] = buffer[size - i - 1];
     buffer[size - i - 1] = tmp;
   }
 }
 
-ssize_t write_buffer(const int fd, const char *buffer, const size_t bsize) {
+void reverse_buffer_bswap(uint8_t *buffer, const size_t size) {
+  if (size % sizeof(uint64_t) == 0) {
+    uint64_t *buf = (uint64_t *)buffer;
+    size_t new_size = size / 8;
+    for (size_t i = 0; i < new_size / 2; ++i) {
+      uint64_t tmp = buf[i];
+      buf[i] = __builtin_bswap64(buf[new_size - i - 1]);
+      buf[new_size - i - 1] = __builtin_bswap64(tmp);
+    }
+  } else
+    reverse_buffer(buffer, size);
+}
+
+ssize_t write_buffer(const int fd, uint8_t *buffer, const size_t bsize) {
   return write(fd, buffer, bsize);
 }
 
@@ -67,7 +84,6 @@ int main(int argc, char *argv[]) {
   char *source = argv[1];
 
   create_destination(source);
-  char *dest = argv[2];
 
   int source_fd, dest_fd;
 
@@ -78,22 +94,26 @@ int main(int argc, char *argv[]) {
 
   off_t total_size = get_file_size(source_fd), processed_size = 0;
 
-  char *buffer = malloc(BUFFER_SIZE);
+  uint8_t *buffer = malloc(BUFFER_SIZE);
 
   if (buffer == NULL)
     handle_error("malloc(BUFFER_SIZE");
 
   off_t prev_offset, new_offset;
-  if ((prev_offset = lseek(source_fd, 0, SEEK_END)) < 0)
+  if ((prev_offset = lseek(source_fd, 0, SEEK_END)) < 0) {
+    free(buffer);
     handle_error("lseek");
+  }
 
   while (prev_offset != 0) {
     new_offset = prev_offset - BUFFER_SIZE;
     if (new_offset < 0)
       new_offset = 0;
 
-    if (lseek(source_fd, new_offset, SEEK_SET) < 0)
+    if (lseek(source_fd, new_offset, SEEK_SET) < 0) {
+      free(buffer);
       handle_error("lseek(source_fd)");
+    }
 
     size_t expected_b = prev_offset - new_offset;
     off_t actual_b = read(source_fd, buffer, expected_b);
@@ -101,14 +121,17 @@ int main(int argc, char *argv[]) {
     if (expected_b != actual_b) {
       char *b = "partial read\n";
       write(STDERR_FILENO, b, strlen(b));
+      free(buffer);
       exit(EXIT_FAILURE);
     }
 
     prev_offset = new_offset;
 
-    reverse_buffer(buffer, actual_b);
-    if (write_buffer(dest_fd, buffer, actual_b) < 0)
+    reverse_buffer_bswap(buffer, actual_b);
+    if (write_buffer(dest_fd, buffer, actual_b) < 0) {
+      free(buffer);
       handle_error("write_buffer");
+    }
 
     processed_size += actual_b;
 
